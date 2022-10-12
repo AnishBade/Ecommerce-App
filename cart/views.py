1733263b687bcfc4e06910664869781a3eac3a64
AnishBade <badeanish6045@gmail.com>
2022-10-12 15:18:03 +0545

    adding products to the cart

--- cart/views.py
+++ cart/views.py
@@ -31,4 +31,4 @@ def cart_detail(request):
     for item in cart:
         item['update_quantity_form'] = CartAddProductForm(initial={'quantity': item['quantity'],
                                                                    'override': True})
-    return render(request, 'cart/detail.html', {'cart': cart})vv
+    return render(request, 'cart/detail.html', {'cart': cart})
